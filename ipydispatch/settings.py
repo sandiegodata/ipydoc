b0ccaf9b81eb85b7f2f58157b4e230993181e57d
Eric Busboom <eric@clarinova.com>
2014-06-21 19:01:54 -0700

    Linking up logout handling

--- ipydispatch/settings.py
+++ ipydispatch/settings.py
@@ -147,7 +147,7 @@ LOGGING = {
 ## Social Auth 
 LOGIN_REDIRECT_URL = '/done'
 
-#SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 
 SOCIAL_AUTH_PIPELINE = (
     'social_auth.backends.pipeline.social.social_auth_user',
